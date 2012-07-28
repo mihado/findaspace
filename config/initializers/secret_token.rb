2bc5f78c4730f269ec4faa44995cb064d22fc61a
mekf <nhat.minh.ha@gmail.com>
2012-07-28 12:19:37 +1200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findaspace::Application.config.secret_token = '519fc2c627c1352a29d3307fabc8c428bb92c2ed897a445743b3972741fa5aecd6d00263cc0cea66cd014be22803acda7a9c11ffba6af0b3130bc2112e8c520f'
