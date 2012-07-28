e80b175253f0ef025fc2e984ba8f17d12d2f2161
mekf <nhat.minh.ha@gmail.com>
2012-07-28 16:30:14 +1200

    validating in /models/space.rb

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,3 +1,14 @@
 class Space < ActiveRecord::Base
   attr_accessible :address, :availability, :capacity, :city, :desc, :img_url, :name, :price, :suburb
+  	validates 	:name,
+  				:address,
+				:suburb,
+				:city,
+				:desc,
+				:capacity,
+				:price,
+				:img_url, :presence => true
+
+	validates 	:price, :numericality => {:greater_than_or_equal_to => 0.01}
+	validates	:capacity, :numericality => {:greater_than_or_equal_to => 1}
 end
